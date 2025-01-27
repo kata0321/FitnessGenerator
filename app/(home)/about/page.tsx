d2c6d0aa1893febb372302c58469319e2b721e79
ettoufah ayoub <tpgare3@gmail.com>
2025-01-27 16:08:46 +0100

    Add files via upload
    home page

---
+++ app/(home)/about/page.tsx
@@ -0,0 +1,43 @@
+export default function AboutPage() {
+  return (
+      <div className="px-6 lg:w-5/6 xl:w-4/6 py-10 mx-auto">
+        <div className="space-y-12 text-md md:text-lg text-gray-500 pb-4 md:pb-8">
+          <div className="space-y-2">
+            <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+              💪 Workout Program
+            </h2>
+            <p>
+              Our workout programs are designed to help you achieve your fitness goals quickly and easily. Whether it's building muscle, increasing endurance, or improving your overall fitness, our diverse workouts provide you with a personalized exercise regimen that fits your needs and schedule.
+            </p>
+          </div>
+
+          <div className="space-y-2">
+            <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+              🥗 Diet Program
+            </h2>
+            <p>
+              Achieve optimal results with our personalized diet programs that complement your workout routine. Our nutrition experts have created meal plans for various fitness goals and preferences, helping you stay on track for your health and well-being.
+            </p>
+          </div>
+
+          <div className="space-y-2">
+            <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+              📊 Weight Analysis
+            </h2>
+            <p>
+              Stay informed about your progress with our advanced weight analysis. Track changes in your weight, set achievable goals, and monitor your weight loss or muscle gain journey. Our detailed analysis provides you with valuable insights to make informed decisions and adjust your fitness program.
+            </p>
+          </div>
+
+          <div className="space-y-2">
+            <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+              🏋️‍♀️ Body Composition Analysis
+            </h2>
+            <p>
+              Understand your body better with our detailed body composition analysis. Track changes in muscle mass, body fat percentage, and overall body composition to see your physical progress. Our analysis helps you make informed choices and adjustments to improve your fitness routine.
+            </p>
+          </div>
+        </div>
+      </div>
+  );
+}
