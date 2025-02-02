c57c584d6635157d444ef0111853870ed0797037
ettoufah ayoub <tpgare3@gmail.com>
2025-02-02 19:29:02 +0100

    Update copy.tsx
    use client

--- components/copy.tsx
+++ components/copy.tsx
@@ -31,14 +31,14 @@ export default function CopyLink({ params }: { params: any }) {
   return (
     <Card className="max-w-lg bg-neutral-50 overflow-hidden">
       <CardHeader>
-        <CardTitle>Shpërndajeni këtë program</CardTitle>
-        <CardDescription>Çdokush mund ta shikojë këtë program.</CardDescription>
+        <CardTitle>Share this program</CardTitle>
+        <CardDescription>Anyone can watch this program.</CardDescription>
       </CardHeader>
       <CardContent>
         <div className="flex flex-col lg:flex-row gap-2 items-center">
           <Input contentEditable={false} value={`https://trajneri.com/program/${params.slug}`} />
           <div className="relative">
-            <Button onClick={copy} className="w-32">Kopjo</Button>
+            <Button onClick={copy} className="w-32">Copy</Button>
             {is_copied && (
               <div className="text-md text-neutral-400 absolute top-0 lg:-top-3/4 lg:animate__animated lg:animate__fadeInUp left-full lg:left-2/4 w-full translate-x-5 lg:translate-x-0">Copied👍</div>
             )}
